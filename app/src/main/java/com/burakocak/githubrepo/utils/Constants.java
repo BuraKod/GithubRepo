12cc62ab47ddee2a3e63ed9ec131214b713ebf7d
Burak <burakocak@teknasyon.com>
2019-08-25 15:10:48 +0300

    added app constans

---
+++ app/src/main/java/com/burakocak/githubrepo/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.burakocak.githubrepo.utils;
+
+public class Constants {
+    public static final String SUCCESS = "success";
+    public static final String FAILED = "failed";
+    public static int SHOW_LOADING = 1005;
+    public static int HIDE_LOADING = 1006;
+    public static final int RESULT_OK = 200;
+    public static final int RESULT_NO = 400;
+    public static final int ADD_TODO_REQUEST=1;
+}
